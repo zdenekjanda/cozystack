e954ab7f8b1dbe37a228d31516d46820c3700d7f
Andrei Kvapil <kvapss@gmail.com>
2025-04-08 23:27:20 +0200

    Upload kernel and initramfs to release assets
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- hack/upload-assets.sh
+++ hack/upload-assets.sh
@@ -7,3 +7,5 @@ gh release upload --clobber $version _out/assets/cozystack-installer.yaml
 gh release upload --clobber $version _out/assets/metal-amd64.iso
 gh release upload --clobber $version _out/assets/metal-amd64.raw.xz
 gh release upload --clobber $version _out/assets/nocloud-amd64.raw.xz
+gh release upload --clobber $version _out/assets/kernel-amd64
+gh release upload --clobber $version _out/assets/initramfs-metal-amd64.xz
